72977e06913dae59925a4b9b0325c71d69690e68
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-02 10:49:09 +0200

    udpate tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -33,15 +33,17 @@ def test_save_Joueur():
 
 
 def test_load_Joueur():
-    """Test to know hot to load a Joueur"""
+    """Test to know hot to load a Joueur
+
+    ==> LOAD ALL JOUERS in A LIST"""
 
     # initialisation d'un joueur
     random_id = "test_" + secrets.token_hex(6)
     joueur = Joueur(random_id, random_id, "01/01/1900", random_id)  # save
     joueur.save()
 
-    output = Joueur.load()
     # output = Joueur().load()
+    output = Joueur.load()
 
     # ==> Voudrais => la liste de tous les joueurs
     assert isinstance(output, list)
@@ -52,7 +54,10 @@ def test_load_Joueur():
 
 
 def test_search_by_id():
-    """test if possible to select a specific Joueur directly with is id"""
+    """test if possible to select a specific Joueur directly with is id
+
+    LOAD JUST THE GOOD PLAYER WITH GOOD ID
+    """
 
     # initialisation d'un joueur + Save
     random_id = "test_" + secrets.token_hex(6)
