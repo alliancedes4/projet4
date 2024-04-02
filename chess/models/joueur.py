63199b59af3441445e77ce35bbcdd47ed5094ed0
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-02 10:49:24 +0200

    add placeholders

--- chess/models/joueur.py
+++ chess/models/joueur.py
@@ -6,6 +6,8 @@ from tinydb import TinyDB, Query
 
 
 class Joueur:
+    """ """
+
     def __init__(
         self,
         nom: str,
@@ -25,21 +27,43 @@ class Joueur:
         db.insert(self.__dict__)
         db.close()
 
+    @classmethod
     def load(self):
-        """Charge le joueur depuis la base de données."""
+        """Charge tous les jouers depuis la base de données."""
 
         db = TinyDB("player.json")
-        Joueur = Query()
-        joueur_cherche = db.get(Joueur.identifiant == self.identifiant)
-        db.close()
-        if joueur_cherche:
-            self.nom = joueur_cherche["nom"]
-            self.prenom = joueur_cherche["prenom"]
-            self.date_naissance = joueur_cherche["date_naissance"]
-            self.identifiant = joueur_cherche["identifiant"]
-            return True
-        else:
-            return False
+        # Joueur = Query()
+        # joueur_cherche = db.get(Joueur.identifiant == self.identifiant)
+        # db.close()
+        # if joueur_cherche:
+        #     self.nom = joueur_cherche["nom"]
+        #     self.prenom = joueur_cherche["prenom"]
+        #     self.date_naissance = joueur_cherche["date_naissance"]
+        #     self.identifiant = joueur_cherche["identifiant"]
+        #     return True
+        # else:
+        #     return False
+
+        list_player = db.all()
+        return list_player
+
+    @classmethod
+    def search_by_id(self, identifiant):
+        """ """
+
+        # >>> from tinydb import TinyDB, Query
+        # >>> db = TinyDB('path/to/db.json')
+        # >>> User = Query()
+        # >>> db.insert({'name': 'John', 'age': 22})
+        # >>> db.search(User.name == 'John')
+        # [{'name': 'John', 'age': 22}]
+
+        #########################################"
+        #   INSERT CODE HERE
+        #   forcement on va utiliser les resultats de la methode précédente
+        #########################################""
+
+        # RETURN AN INSTANCE OF JOUEUR
 
     def __repr__(self) -> str:
 
