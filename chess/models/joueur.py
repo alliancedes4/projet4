c60c7eb7f6c0d8df8aa67dfe54321c8f54f370ae
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-02 10:33:59 +0200

    udpate filename and imports

--- chess/models/joueur.py
+++ chess/models/joueur.py
@@ -1,29 +1,42 @@
+"""
+Class Player
+"""
+
+from tinydb import TinyDB, Query
+
+
 class Joueur:
-    def __init__(self, nom, prenom, date_naissance, identifiant):
+    def __init__(
+        self,
+        nom: str,
+        prenom: str,
+        date_naissance: str,
+        identifiant: str,
+    ):
         self.nom = nom
         self.prenom = prenom
         self.date_naissance = date_naissance
         self.identifiant = identifiant
 
-    def save(self) :
-            """Sauvegarde le joueur dans la base de données."""
-            from tinydb import TinyDB, Query
-            db = TinyDB('db.json')
-            db.insert( self.__dict__)
-            db.close()
+    def save(self):
+        """Sauvegarde le joueur dans la base de données."""
 
-    def load(self) :
+        db = TinyDB("player.json")
+        db.insert(self.__dict__)
+        db.close()
+
+    def load(self):
         """Charge le joueur depuis la base de données."""
-        from tinydb import TinyDB, Query
-        db = TinyDB('db.json')
+
+        db = TinyDB("player.json")
         Joueur = Query()
         joueur_cherche = db.get(Joueur.identifiant == self.identifiant)
         db.close()
         if joueur_cherche:
-            self.nom = joueur_cherche['nom']
-            self.prenom = joueur_cherche['prenom']
-            self.date_naissance = joueur_cherche['date_naissance']
-            self.identifiant = joueur_cherche['identifiant']
+            self.nom = joueur_cherche["nom"]
+            self.prenom = joueur_cherche["prenom"]
+            self.date_naissance = joueur_cherche["date_naissance"]
+            self.identifiant = joueur_cherche["identifiant"]
             return True
         else:
             return False
